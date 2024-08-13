4e9281ec8df3fcd7bfd5900bdbc66ec3631e1ccf
Vlad-Andrei Badoiu <vlad_andrei.badoiu@upb.ro>
2024-08-13 21:21:42 +0000

    Make doc extraction multiprocess

--- README.md
+++ README.md
@@ -14,14 +14,7 @@ cd src/textit/processors && mkdir -p lang_id && cd lang_id && touch __init__.py 
 ## Usage
 
 The following code turns all the files from `tests/fixtures` int json files in `extracted_text`.
+
 ```Bash
 python use_extractor.py tests/fixtures  extracted_text/
-```
-
-## Limitations
-
-Currently, we can process only one doc at a time due to libreoffice. The solution is to create several user profiles and use them separately:
-
-```
-soffice --headless -env:UserInstallation=file:///tmp/test2
 ```
--- src/textit/extractors/doc_extractor.py
+++ src/textit/extractors/doc_extractor.py
@@ -5,33 +5,43 @@ from textit.helpers import Result
 import subprocess
 import os
 from multiprocessing import Lock
-
-doc_lock = Lock()
+import tempfile
+import shutil
+#doc_lock = Lock()
 
 def doc_handler(file_path: str, metadata: Metadata) -> tuple[Result[List[str]], Metadata]:
     try:
         # Sadly we can only process one doc at a time
-        with doc_lock:
-            file_name = os.path.splitext(os.path.basename(file_path))[0]
-            output_file = f"{file_name}.txt"
+        #with doc_lock:
+        tmpdir = tempfile.mkdtemp()
+        file_name = os.path.splitext(os.path.basename(file_path))[0]
+        output_file = f"{file_name}.txt"
+        user_install_arg = f"-env:UserInstallation=file://{os.path.abspath(tmpdir)}"
+        # Run the LibreOffice command
+        try:
+            result = subprocess.run(["soffice",
+                                     "--headless",
+                                     "--invisible",
+                                     "--nodefault",
+                                     "--norestore", 
+                                     user_install_arg,
+                                     "--convert-to", "txt:Text", file_path], 
+                                    check=True, capture_output=True, text=True, timeout=35)
+        except subprocess.CalledProcessError as e:
+            shutil.rmtree(tmpdir) 
+            raise RuntimeError(f"Error converting file - {file_path}: {e.stderr}")
 
-            # Run the LibreOffice command
-            try:
-                result = subprocess.run(["soffice", "--headless", "--invisible", "--nodefault", "--norestore", "--convert-to", "txt:Text", file_path], 
-                                        check=True, capture_output=True, text=True, timeout=10)
-            except subprocess.CalledProcessError as e:
-                raise RuntimeError(f"Error converting file - {file_path}: {e.stderr}")
-            
-            # Read the content of the output file
-            try:
-                with open(output_file, 'r') as f:
-                    text_content = f.read()
-            except IOError as e:
-                raise RuntimeError(f"Error reading output file: {str(e)}")
-            
-            # Remove the temporary output file
+        shutil.rmtree(tmpdir) 
+        # Read the content of the output file
+        try:
+            with open(output_file, 'r') as f:
+                text_content = f.read()
+        except IOError as e:
             os.remove(output_file)
-            return Result.ok([text_content])
+            raise RuntimeError(f"Error reading output file: {str(e)}")
+        
+        # Remove the temporary output file
+        os.remove(output_file)
+        return Result.ok([text_content]), metadata
     except Exception as e:
-        print(e)
-        return Result.err(f"Error extracting text from DOC/X at {file_path}: {str(e)}")
+        return Result.err(f"Error extracting text from DOC/X at {file_path}: {str(e)}"), metadata
