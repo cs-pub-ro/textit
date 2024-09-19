71d478e6c3802d9b54f7851f39d227ad67bc460e
Mihai-Valentin DUMITRU <mihai.dumitru2201@upb.ro>
2024-09-19 15:13:21 +0000

    allow view_text to print to stdout

--- tools/view_text.py
+++ tools/view_text.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 import json
 import sys
+import argparse
 
 
 def main():
-    assert len(sys.argv) == 3, "Usage: ./extract <input-json> <output-txt>!"
-    with open(sys.argv[1], "r") as fin:
+    parser = argparse.ArgumentParser(description="Get quality statistics")
+    parser.add_argument("src", help="File to read")
+    parser.add_argument("--out", help="Path where to write human-readable text (if absent, will write to stdout).")
+
+    args = parser.parse_args()
+    with open(args.src, "r") as fin:
         js = json.load(fin)
 
     assert "raw_content" in js, "Malformed json, missing 'raw_content'!"
-    with open(sys.argv[2], "w") as fout:
-        fout.write(js["raw_content"])
+
+    if args.out:
+        with open(args.out, "w") as fout:
+            fout.write(js["raw_content"])
+    else:
+        print(js["raw_content"])
 
 
 if __name__ == "__main__":
